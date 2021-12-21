43659f8badae63fcce3692b2c8274f6545bb5e4d
Christopher Gustavo Sanchez Calderon <s.christopher3@gmail.com>
2021-12-21 02:03:00 -0500

    first commit

---
+++ src/test/java/com/programmerfriend/ultimateredis/UltimateRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmerfriend.ultimateredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UltimateRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
